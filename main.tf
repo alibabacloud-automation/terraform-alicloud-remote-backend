9cb266d1204ed934327fc9b91a21a0e8bc7d2611
zou2699 <zou2699@gmail.com>
2021-10-27 15:56:16 +0800

    fix provider warning

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@
 #  configuration_source = "terraform-alicloud-modules/remote-backend"
 #}
 
+terraform {
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+    }
+  }
+}
+
+
 locals {
   default_bucket_name     = "terraform-remote-backend-${random_uuid.this.result}"
   default_lock_table_name = replace("terraform-remote-backend-lock-table-${random_uuid.this.result}", "-", "_")
